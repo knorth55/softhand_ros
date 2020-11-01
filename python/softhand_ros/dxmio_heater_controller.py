4a8d4e7ff7fb121cf95735756823f5fe76702d5e
Toshinori Hirose <hirose.toshi@gmail.com>
2020-11-01 15:56:26 +0900

    add code for DXMIO heater control

--- python/softhand_ros/dxmio_heater_controller.py
+++ python/softhand_ros/dxmio_heater_controller.py
@@ -16,6 +16,12 @@ from dynamixel_msgs.msg import MotorStateList
 from dynamixel_msgs.msg import JointState
 
 
+# Control Table Constants for DXMIO
+DXMIO_PWM_DUTY_0 = 22
+DXMIO_PWM_DUTY_1 = 24
+DXMIO_PWM_DUTY_2 = 26
+
+
 class DxmioHeaterController(object):
     # initialize, start, stop
     def __init__(self, dxl_io, controller_namespace, port_namespace):
@@ -34,8 +40,9 @@ class DxmioHeaterController(object):
     def start(self):
         self.running = True
         self.joint_state_pub = rospy.Publisher(self.controller_namespace + '/state', JointState, queue_size=1)
-        self.command_sub = rospy.Subscriber(self.controller_namespace + '/command', Float64, self.process_command)
-        self.motor_states_sub = rospy.Subscriber('motor_states/%s' % self.port_namespace, MotorStateList, self.process_motor_states)
+        self.command1_sub = rospy.Subscriber(self.controller_namespace + '/command1', Float64, self.process_command1)
+        self.command2_sub = rospy.Subscriber(self.controller_namespace + '/command2', Float64, self.process_command2)
+        self.command3_sub = rospy.Subscriber(self.controller_namespace + '/command3', Float64, self.process_command3)        
 
     def stop(self):
         self.running = False
@@ -46,9 +53,22 @@ class DxmioHeaterController(object):
         self.torque_service.shutdown('normal shutdown')
         self.compliance_slope_service.shutdown('normal shutdown')
 
-    def set_pwm_duty(self, pwm_duty):
-        raise NotImplementedError
+    def set_pwm_duty(self, address, pwm_duty):
+        sid = self.motor_id
+        pwm_duty = min (pwm_duty, 0.1) # PWM duty limit for circuit protection
+        pwm_duty *= 65535 # PWM duty conversion 1.0 -> 65535
+        pwm_duty &= 0xffff
+        loVal = int(pwm_duty % 256) # split pwm_duty into 2 bytes
+        hiVal = int(pwm_duty >> 8)
+        writeableVals = [] # prepare value tuples for call to syncwrite
+        writeableVals.append( (sid, loVal, hiVal) )
+        self.sync_write(address, writeableVals) # use sync write to broadcast multi servo message
 
-    def process_set_pwm_duty(self, req):
-        self.set_pwm_duty(req.pwm_duty)
-        return [] # success
+    def process_command1(self, msg):
+        self.set_pwm_duty(DXMIO_PWM_DUTY_0, msg.data)
+
+    def process_command2(self, msg):
+        self.set_pwm_duty(DXMIO_PWM_DUTY_1, msg.data)
+
+    def process_command3(self, msg):
+        self.set_pwm_duty(DXMIO_PWM_DUTY_2, msg.data)
