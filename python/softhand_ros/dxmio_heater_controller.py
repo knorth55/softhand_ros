78b5a24d8091dc649b7f4ff0a24a037b756b432a
Shingo Kitagawa <shingogo@hotmail.co.jp>
2020-11-08 02:16:39 +0900

    flake8

--- python/softhand_ros/dxmio_heater_controller.py
+++ python/softhand_ros/dxmio_heater_controller.py
@@ -1,21 +1,11 @@
-import math
 import threading
 
 import rospy
 
-from dynamixel_driver.dynamixel_const import *
+# from dynamixel_driver.dynamixel_const import *
 
-from dynamixel_controllers.srv import SetSpeed
-from dynamixel_controllers.srv import TorqueEnable
-from dynamixel_controllers.srv import SetComplianceSlope
-from dynamixel_controllers.srv import SetComplianceMargin
-from dynamixel_controllers.srv import SetCompliancePunch
-from dynamixel_controllers.srv import SetTorqueLimit
-
-from std_msgs.msg import Float64
-from dynamixel_msgs.msg import MotorStateList
 from dynamixel_msgs.msg import JointState
-
+from std_msgs.msg import Float64
 
 # Control Table Constants for DXMIO
 DXMIO_PWM_DUTY_0 = 22
@@ -30,22 +20,32 @@ class DxmioHeaterController(object):
         self.dxl_io = dxl_io
         self.controller_namespace = controller_namespace
         self.port_namespace = port_namespace
-        self.motor_id = rospy.get_param(self.controller_namespace + '/motor/id')
-        self.joint_name = rospy.get_param(self.controller_namespace + '/joint_name')
+        self.motor_id = rospy.get_param(
+            self.controller_namespace + '/motor/id')
+        self.joint_name = rospy.get_param(
+            self.controller_namespace + '/joint_name')
         self.lock = threading.Lock()
 
         # joint_state
-        self.joint_state = JointState(name=self.joint_name, motor_ids=[self.motor_id])
+        self.joint_state = JointState(
+            name=self.joint_name, motor_ids=[self.motor_id])
 
     def initialize(self):
         return True
 
     def start(self):
         self.running = True
-        self.joint_state_pub = rospy.Publisher(self.controller_namespace + '/state', JointState, queue_size=1)
-        self.command1_sub = rospy.Subscriber(self.controller_namespace + '/command1', Float64, self.process_command1)
-        self.command2_sub = rospy.Subscriber(self.controller_namespace + '/command2', Float64, self.process_command2)
-        self.command3_sub = rospy.Subscriber(self.controller_namespace + '/command3', Float64, self.process_command3)        
+        self.joint_state_pub = rospy.Publisher(
+            self.controller_namespace + '/state', JointState, queue_size=1)
+        self.command1_sub = rospy.Subscriber(
+            self.controller_namespace + '/command1',
+            Float64, self.process_command1)
+        self.command2_sub = rospy.Subscriber(
+            self.controller_namespace + '/command2',
+            Float64, self.process_command2)
+        self.command3_sub = rospy.Subscriber(
+            self.controller_namespace + '/command3',
+            Float64, self.process_command3)
 
     def stop(self):
         self.running = False
@@ -58,17 +58,19 @@ class DxmioHeaterController(object):
 
     def set_pwm_duty(self, address, pwm_duty):
         sid = self.motor_id
-        pwm_duty = min (pwm_duty, 0.1) # PWM duty limit for circuit protection
-        pwm_duty = int(65535 * pwm_duty) # PWM duty conversion 1.0 -> 65535
+        # PWM duty limit for circuit protection
+        pwm_duty = min(pwm_duty, 0.1)
+        # PWM duty conversion 1.0 -> 65535
+        pwm_duty = int(65535 * pwm_duty)
         pwm_duty &= 0xffff
-        loVal = int(pwm_duty % 256) # split pwm_duty into 2 bytes
-        hiVal = int(pwm_duty >> 8)
-        # writeableVals = [] # prepare value tuples for call to syncwrite
-        # writeableVals.append( (sid, loVal, hiVal) )
-        self.dxl_io.write(sid, address, (loVal, hiVal)) # use sync write to broadcast multi servo message
+        # split pwm_duty into 2 bytes
+        lo_val = int(pwm_duty % 256)
+        hi_val = int(pwm_duty >> 8)
+        # use write to broadcast multi servo message
+        self.dxl_io.write(sid, address, (lo_val, hi_val))
         rospy.loginfo('address: {}'.format(address))
         rospy.loginfo('pwm_duty: {}'.format(pwm_duty))
-        
+
     def process_command1(self, msg):
         with self.lock:
             self.set_pwm_duty(DXMIO_PWM_DUTY_0, msg.data)
