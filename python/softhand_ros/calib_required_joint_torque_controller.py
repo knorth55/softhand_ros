bea8a8822d23c5b5a6044dac2ee646569122da39
Shingo Kitagawa <shingogo@hotmail.co.jp>
2021-07-23 16:19:45 +0900

    add CalibRequiredJointTorqueController

---
+++ python/softhand_ros/calib_required_joint_torque_controller.py
@@ -0,0 +1,13 @@
+from dynamixel_controllers.joint_position_controller \
+    import JointTorqueController
+
+from softhand_ros.calib_required_controller import CalibRequiredController
+
+
+class CalibRequiredJointTorqueController(
+        JointTorqueController, CalibRequiredController
+):
+    def __init__(self, dxl_io, controller_namespace, port_namespace):
+        JointTorqueController.__init__(
+            self, dxl_io, controller_namespace, port_namespace)
+        CalibRequiredController.__init__(self)
